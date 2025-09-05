9ab1e02733be1b862767ac4386554f197a6424dd
oskarmmd <osde0001@stud.kea.dk>
2025-09-05 11:14:53 +0200

    jepjep

--- 3capitalization/capitalization3.js
+++ 3capitalization/capitalization3.js
@@ -1,7 +1,16 @@
 const firstName = "Peter";
 const lower = firstName.toLowerCase();
-const midIndex = Math.floor(firstName.length / 2);
-const capLetter = lower[midIndex].toUpperCase();
 
-const result = lower.slice(0, midIndex) + capLetter + lower.slice(midIndex + 1);
+// løsning midterbogstav
+// const midIndex = Math.floor(firstName.length / 2);
+// const capLetter = lower[midIndex].toUpperCase();
+
+// const result = lower.slice(0, midIndex) + capLetter + lower.slice(midIndex + 1);
+// console.log(result);
+
+// løsning første bogstav
+const firstIndex = 0;
+const capLetter = lower[firstIndex].toUpperCase();
+
+const result = capLetter + lower.slice(1);
 console.log(result);
